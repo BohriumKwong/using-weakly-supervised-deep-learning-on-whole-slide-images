1ed861ec680851fc5de92874b053162ef57b31cb
BohriumKwong <bohrium.kwong@gmail.com>
2020-06-01 15:43:23 +0800

    增加group_log_excel,用于保存整个batch的slide预测结果

--- mark_result.py
+++ mark_result.py
@@ -1,9 +1,8 @@
-#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
 Created on Fri Mar  6 13:53:14 2020
 
-@author: root
+@author: Bohrium.Kwong
 """
 import pandas as pd
 import os
@@ -21,13 +20,19 @@ def result_excel(dset,pred,save_name):
     if not os.path.isdir(save_dir): os.makedirs(save_dir)
     test.to_excel(os.path.join(save_dir,save_name +'.xlsx'),index = False)
     
-def result_excel_origin(dset,pred,save_name):
+def result_excel_origin(dset,pred,save_name,dir_name = 'densenet'):
     file_list = []
     for i in dset.slidenames:
        file_list.append(os.path.basename(i)) 
     
     c = {'file_name':file_list,'predict':list(pred),'true_label':dset.targets}
     test = pd.DataFrame(c)
-    save_dir = os.path.join('output','result')
+    save_dir = os.path.join('output','result',dir_name)
     if not os.path.isdir(save_dir): os.makedirs(save_dir)
     test.to_excel(os.path.join(save_dir,save_name +'.xlsx'),index = False)
+
+def group_log_excel(result_dict,save_name):
+    result = pd.DataFrame(result_dict)
+    save_dir = os.path.join('output','result','batch_metric')
+    if not os.path.isdir(save_dir): os.makedirs(save_dir)
+    result.to_excel(os.path.join(save_dir,save_name +'.xlsx'),index = False)
