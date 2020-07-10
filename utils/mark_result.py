50cc9c1ad31db4c52fb72ab7ce66b50d8446e3e8
BohriumKwong <bohrium.kwong@gmail.com>
2020-07-10 11:20:26 +0800

    迁移文件

--- utils/mark_result.py
+++ utils/mark_result.py
@@ -1,8 +1,9 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
 Created on Fri Mar  6 13:53:14 2020
 
-@author: Bohrium.Kwong
+@author: root
 """
 import pandas as pd
 import os
@@ -20,19 +21,19 @@ def result_excel(dset,pred,save_name):
     if not os.path.isdir(save_dir): os.makedirs(save_dir)
     test.to_excel(os.path.join(save_dir,save_name +'.xlsx'),index = False)
     
-def result_excel_origin(dset,pred,save_name,dir_name = 'densenet'):
+def result_excel_origin(dset,pred,save_name,save_dir='output'):
     file_list = []
     for i in dset.slidenames:
        file_list.append(os.path.basename(i)) 
     
     c = {'file_name':file_list,'predict':list(pred),'true_label':dset.targets}
     test = pd.DataFrame(c)
-    save_dir = os.path.join('output','result',dir_name)
+#    save_dir = os.path.join('output','3_FOLD',dir_name)
     if not os.path.isdir(save_dir): os.makedirs(save_dir)
     test.to_excel(os.path.join(save_dir,save_name +'.xlsx'),index = False)
-
-def group_log_excel(result_dict,save_name):
+    
+def group_log_excel(result_dict,save_name,save_dir=''):
     result = pd.DataFrame(result_dict)
-    save_dir = os.path.join('output','result','batch_metric')
+#    save_dir = os.path.join('output','dataset_result','MIL','densenet')
     if not os.path.isdir(save_dir): os.makedirs(save_dir)
     result.to_excel(os.path.join(save_dir,save_name +'.xlsx'),index = False)
